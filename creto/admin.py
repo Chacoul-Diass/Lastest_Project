44d20fa03257c4264191c58c178c28703f8d28a0
225junior <225juniorbrindou@gmail.com>
2020-07-22 18:03:54 +0000

    creation du model creto

--- creto/admin.py
+++ creto/admin.py
@@ -1,3 +1,37 @@
 from django.contrib import admin
+from creto import models 
 
 # Register your models here.
+
+
+
+class CretoAdmin(admin.ModelAdmin):
+	list_display = (
+		'title',
+		'telephone',
+		'telephone2',
+		'email',
+		'email2',
+		'address',
+		'date_cre',
+		'date_mod',
+	)
+
+	fieldsets = [
+		('Info', {
+			'fields':[
+				'title',
+				'telephone',
+				'telephone2',
+				'email',
+				'email2',
+				'address',
+					]
+		}),
+	]
+
+
+def _register(model, admin_class):
+    admin.site.register(model, admin_class)
+
+_register(models.Creto, CretoAdmin)
--- creto/models.py
+++ creto/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
 # Create your models here.
+
+class Creto(models.Model):
+
+	title = models.CharField(default='best bikes for you', max_length=50)
+	# numero de telephone 1
+	telephone = models.CharField(default='99999999', max_length=8)
+	# numero de telephone 2
+	telephone2 = models.CharField(default='99999999', max_length=8)
+
+	email = models.CharField(default='creto@gmail.com', max_length=50)
+
+
+	email2 = models.CharField(default='rovadex@gmail.com', max_length=50)
+
+	address = models.CharField(max_length=255, default='Rovadex © 2020. All Rights Reserved')
+
+	date_cre = models.DateTimeField(auto_now_add=True)
+	date_mod = models.DateTimeField(auto_now=True)
+
+	class Meta:
+		verbose_name = "Creto"
+		verbose_name_plural = "Creto Informations"
+
+	def __str__(self):
+		return self.title
