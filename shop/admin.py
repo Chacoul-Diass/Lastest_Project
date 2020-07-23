640e4442f92435d2da593c0eea6d5c03bb148e69
225junior <225juniorbrindou@gmail.com>
2020-07-23 00:00:46 +0000

    creation de shop model

--- shop/admin.py
+++ shop/admin.py
@@ -1,3 +1,83 @@
 from django.contrib import admin
+from shop import models 
 
 # Register your models here.
+
+class CategoryAdmin(admin.ModelAdmin):
+	list_display = (
+		'titre',
+		'status',
+		'date_cre',
+		'date_mod',
+	)
+	list_filter = (
+		'titre',
+	)
+	search_fields = (
+		'titre',
+	)
+	list_per_page = 10
+	fieldsets = [
+		('Info', {
+			'fields':[
+				'titre',
+			]
+		}),
+		('Status et Activation', {
+			'fields':[
+				'status',
+			]
+		})
+	]
+
+class ProductAdmin(admin.ModelAdmin):
+	list_display = (
+		'titre',
+		'prix',
+		'reduction',
+		'image0',
+		'image1',
+		'image2',
+		'image3',
+		'categorie',
+		'status',
+		'recommande',
+	)
+	list_filter = (
+		'categorie',
+	)
+	search_fields = (
+		'titre',
+	)
+	list_per_page = 10
+	fieldsets = [
+		('Info', {
+			'fields':[
+				'titre',
+				'prix',
+				'reduction',
+				'image0',
+				'image1',
+				'image2',
+				'image3',
+				'categorie',
+				'description',
+
+
+			]
+		}),
+
+		('Status et Activation', {
+			'fields':[
+				'recommande',
+				'status',
+			]
+		})
+	]
+
+def _register(model, admin_class):
+    admin.site.register(model, admin_class)
+
+
+_register(models.Category, CategoryAdmin)
+_register(models.Product, ProductAdmin)
--- shop/models.py
+++ shop/models.py
@@ -1,3 +1,43 @@
 from django.db import models
 
 # Create your models here.
+
+class Category(models.Model):
+	titre = models.CharField(max_length=255)
+	status = models.BooleanField(default=True)
+
+	date_cre = models.DateTimeField(auto_now_add=True)
+	date_mod = models.DateTimeField(auto_now=True)
+
+	class Meta:
+		verbose_name = "Categorie"
+		verbose_name_plural = "Les Categories"
+
+	def __str__(self):
+		return self.titre
+
+
+class Product(models.Model):
+	titre = models.CharField(max_length=255)
+	detais = models.CharField(max_length=255)
+	image0 = models.ImageField(upload_to = 'shop/produit/image', blank=True, null =True)
+	image1 = models.ImageField(upload_to = 'shop/produit/image', blank=True, null =True)
+	image2 = models.ImageField(upload_to = 'shop/produit/image', blank=True, null =True)
+	image3 = models.ImageField(upload_to = 'shop/produit/image', blank=True, null =True)
+	prix = models.FloatField()
+	reduction = models.FloatField(blank=True)
+	categorie = models.ForeignKey(Category, on_delete=models.CASCADE, related_name = 'categorie')
+	status = models.BooleanField(default=True)
+	recommande = models.BooleanField(default=False)
+	description = models.TextField(default='Aucune description')
+
+
+	date_cre = models.DateTimeField(auto_now_add=True)
+	date_mod = models.DateTimeField(auto_now=True)
+
+	class Meta:
+		verbose_name = "product"
+		verbose_name_plural = "Les Products"
+
+	def __str__(self):
+		return self.titre
