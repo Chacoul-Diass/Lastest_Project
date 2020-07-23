ddaecc478dc08959c613ff2d7157600ca59fce2b
Chacoul-Diass <57750236+Chacoul-Diass@users.noreply.github.com>
2020-07-23 21:58:15 +0000

    Update url

--- shop/urls.py
+++ shop/urls.py
@@ -7,12 +7,9 @@ app_name = "shop"
 urlpatterns = [
     path('shop', views.shop, name='shop'),
 
-    # path('single-product/<int:id>/', views.single_product, name="single-product"),
-
     # AddtoCart
     path('addtocart/<int:id>', views.add_to_cart, name='addtocart'),
 
-
-    path('single', views.single, name='single'),
+    path('single/<int:id>', views.single, name='single'),
 	
 ]
